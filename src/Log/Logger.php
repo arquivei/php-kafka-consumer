72ea3fceb008c8761975f477b7a90d1fed379256
Andre Lazari <andre.lazari@arquivei.com.br>
2020-07-07 08:13:12 -0300

    Improve log message

--- src/Log/Logger.php
+++ src/Log/Logger.php
@@ -6,13 +6,15 @@ class Logger
 {
     public function error(?int $messageId, int $attempts, \Throwable $exception): void
     {
-        $error = json_encode([
-            'message' => 'PHP-KAFKA-CONSUMER-ERROR',
-            'message_id' => $messageId,
-            'throwable' => $exception,
-            'attempt' => $attempts,
-            'time' => date('Y-m-d H:i:s'),
-        ]);
+        $error = json_encode(
+            [
+                'message' => 'PHP-KAFKA-CONSUMER-ERROR',
+                'message_id' => $messageId,
+                'throwable' => ["message" => $exception->getMessage(), "exception" => get_class($exception)],
+                'attempt' => $attempts,
+                'time' => date('Y-m-d H:i:s'),
+            ]
+        );
         print $error . PHP_EOL;
     }
 }
